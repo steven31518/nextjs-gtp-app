cc95c439e6e769b9718a38c81fdeebd02cae21d5
steven31518 <107013204+steven31518@users.noreply.github.com>
2024-01-11 12:32:06 +0800

    openAI setup and finish chat completion

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -3,29 +3,51 @@
 import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { generateChatResponse } from "@/utils/action";
+import toast from "react-hot-toast";
+import type { ChatCompletionMessage } from "openai/resources/index.mjs";
 
 export default function Chat() {
   const [text, setText] = useState<string>("");
-  const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<ChatCompletionMessage[]>([]);
 
-  const { mutate, data } = useMutation({
-    mutationFn: (message: string) => generateChatResponse(message),
-    onError: (error) => {
-      console.log(error.message);
-    },
+  const { mutate, data, isPending } = useMutation({
+    mutationFn: (query: ChatCompletionMessage) =>
+      generateChatResponse([...messages, query]),
+
     onSuccess: (data) => {
-      console.log(data);
+      if (!data) {
+        toast.error("Error");
+        return;
+      }
+      setMessages((pre) => [...pre, data]);
     },
   });
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    mutate(text);
+    const query = { role: "user", content: text } as ChatCompletionMessage;
+    mutate(query);
+    setMessages((prev) => [...prev, query]);
+    setText("");
   };
 
   return (
     <div className="min-h-[calc(100vh-6rem)] grid grid-rows-[1fr,auto]">
       <div>
-        <h2 className="text-5xl">{data}</h2>
+        {messages.map(({ role, content }, i) => {
+          const avatar = role == "user" ? "👤" : "🤖";
+          const bcg = role == "user" ? "bg-base-200" : "bg-base-100";
+          return (
+            <div
+              key={i}
+              className={`${bcg} flex py-6 -mx-8 px-8 text-xl leading-loose border-b border-base-300`}
+            >
+              <span className="mr-4 ">{avatar}</span>
+              <p className="max-w-3xl">{content}</p>
+            </div>
+          );
+        })}
+        {isPending && <span className="loading"></span>}
       </div>
       <form onSubmit={handleSubmit} className="max-w-4xl pt-12">
         <div className="join w-full">
@@ -42,8 +64,9 @@ export default function Chat() {
           <button
             className="btn btn-secondary join-item capitalize"
             type="submit"
+            disabled={isPending}
           >
-            ask question
+            {isPending ? "loading..." : " ask question"}
           </button>
         </div>
       </form>
--- utils/action.ts
+++ utils/action.ts
@@ -1,6 +1,25 @@
 "use server";
+import OpenAI from "openai";
+import type { ChatCompletionMessage } from "openai/resources/index.mjs";
 
-export async function generateChatResponse(chatMessage: string) {
-  console.log("chatMessage", chatMessage.split(" "));
-  return "awesome";
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function generateChatResponse(
+  chatMessages: ChatCompletionMessage[]
+) {
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        ...chatMessages,
+      ],
+      model: "gpt-3.5-turbo",
+      temperature: 0,
+    });
+    return response.choices[0].message;
+  } catch (error) {
+    return null;
+  }
 }
